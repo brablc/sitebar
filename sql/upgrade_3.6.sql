26d908065a6ad28a9cd5678e1ff0197255e66223
Ondrej Brablc <ondrej@brablc.com>
2018-08-28 21:02:25 +0200

    Fix sitebar_session.created column before changing ip.

--- sql/upgrade_3.6.sql
+++ sql/upgrade_3.6.sql
@@ -1,5 +1,10 @@
+SET @@sql_mode = '';
+
 ALTER TABLE `sitebar_user` 
 	CHANGE `last_ip` `last_ip` VARCHAR(45) NULL DEFAULT NULL;
+
+ALTER TABLE `sitebar_session`
+    CHANGE `created` `created` DATETIME NULL DEFAULT NULL;
 
 ALTER TABLE `sitebar_session` 
 	CHANGE `ip` `ip` VARCHAR(45) NOT NULL;
